c94472bffc6c9de496b04c3c2c74f99199214be1
Irfan Mohd Rafik Pathan <67030032+irfanp056@users.noreply.github.com>
2026-01-01 11:17:50 +0530

    Delete Shared1 directory

--- Shared1/Models/CustomerBasket.cs
+++
@@ -1,8 +0,0 @@
-// Models/CustomerBasket.cs
-namespace AspireApp2.Shared1.Models;
-
-public class CustomerBasket
-{
-    public string BuyerId { get; set; } = string.Empty;
-    public List<BasketItem> Items { get; set; } = [];
-}
